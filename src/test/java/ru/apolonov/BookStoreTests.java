83a3d84004879e645dc4587dfb4926fa0cd1a4c8
vasvap <vasvap@gmail.com>
2022-01-08 14:46:34 +0300

    tests with logs added

---
+++ src/test/java/ru/apolonov/BookStoreTests.java
@@ -0,0 +1,39 @@
+package ru.apolonov;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+
+public class BookStoreTests {
+
+    @Test
+    void noLogsTest() {
+        given()
+                .get("https://demoqa.com/BookStore/v1/Books")
+                .then()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void allLogsTest() {
+        given()
+                //.log().all()
+                .get("https://demoqa.com/BookStore/v1/Books")
+                .then()
+                .log().all()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void withSomeLogsTest() {
+        given()
+                .log().body()
+                .get("https://demoqa.com/BookStore/v1/Books")
+                .then()
+                .log().body()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+}
