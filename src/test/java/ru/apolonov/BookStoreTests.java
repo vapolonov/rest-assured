60827339d9070dd3b8c6cb4f93cffb6068050266
vasvap <vasvap@gmail.com>
2022-01-09 13:38:33 +0300

    tests with Allure reports added

--- src/test/java/ru/apolonov/BookStoreTests.java
+++ src/test/java/ru/apolonov/BookStoreTests.java
@@ -1,10 +1,16 @@
 package ru.apolonov;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+import static ru.apolonov.filters.CustomLogFilter.customLogFilter;
 
 public class BookStoreTests {
 
@@ -36,4 +42,74 @@ public class BookStoreTests {
                 .body("books", hasSize(greaterThan(0)));
     }
 
+    @Test
+    void authorizeTest() {
+        String data = "{" +
+                "  \"userName\": \"alex\"," +
+                "  \"password\": \"asdsad#frew_DFS2\"" +
+                "}";
+
+        // Map<String, String> data = new HashMap<>();
+        // data.put("userName", "alex");
+        // data.put("password", "asdsad#frew_DFS2");
+
+        given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body(data)
+                .when()
+                .log().uri()
+                .log().body()
+                .post("https://demoqa.com/Account/v1/GenerateToken")
+                .then()
+                .log().body()
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."));
+    }
+
+    @Test
+    void authorizeWithListenerTest() {
+        String data = "{" +
+                "  \"userName\": \"alex\"," +
+                "  \"password\": \"asdsad#frew_DFS2\"" +
+                "}";
+        given()
+                .filter(new AllureRestAssured())
+                .contentType("application/json")
+                .accept("application/json")
+                .body(data)
+                .when()
+                .log().uri()
+                .log().body()
+                .post("https://demoqa.com/Account/v1/GenerateToken")
+                .then()
+                .log().body()
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."));
+    }
+
+    @Test
+    void authorizeWithTemplatesTest() {
+        String data = "{" +
+                "  \"userName\": \"alex\"," +
+                "  \"password\": \"asdsad#frew_DFS2\"" +
+                "}";
+
+        step("Generate token", () ->
+                given()
+                        .filter(customLogFilter().withCustomTemplates())
+                        .contentType("application/json")
+                        .accept("application/json")
+                        .body(data)
+                        .when()
+                        .log().uri()
+                        .log().body()
+                        .post("https://demoqa.com/Account/v1/GenerateToken")
+                        .then()
+                        .log().body()
+                        .body("status", is("Success"))
+                        .body("result", is("User authorized successfully."))
+        );
+        step("Some UI action");
+    }
 }
